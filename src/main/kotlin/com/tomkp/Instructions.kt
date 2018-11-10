e2b0ef8101fa5ac40843df783e1c1cae28aaaf34
tomkp <tom@tomkp.com>
2018-11-10 22:51:40 +0000

    ...

--- src/main/kotlin/com/tomkp/Instructions.kt
+++ src/main/kotlin/com/tomkp/Instructions.kt
@@ -5,6 +5,8 @@ import com.tomkp.Instruction.*
 enum class Instruction { L, R, F }
 
 data class Instructions(val instructions: List<Instruction>) {
+
     constructor(vararg instructions: Instruction) : this(instructions.toList())
+
     constructor(instructions: String) : this(instructions.replace(" ", "").toCharArray().map { valueOf(it.toString()) })
 }
