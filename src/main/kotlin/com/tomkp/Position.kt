bf319956ad91df2f652ee82251a979b111ca2789
tomkp <tom@tomkp.com>
2018-11-10 22:03:51 +0000

    ...

--- src/main/kotlin/com/tomkp/Position.kt
+++ src/main/kotlin/com/tomkp/Position.kt
@@ -1,33 +1,35 @@
 package com.tomkp
 
+import com.tomkp.Orientation.*
+
 data class Position(val coordinate: Coordinate, val orientation: Orientation) {
 
     fun forward(): Position {
         val newCoordinate = when (orientation) {
-            Orientation.N -> Coordinate(coordinate.x, coordinate.y + 1)
-            Orientation.E -> Coordinate(coordinate.x + 1, coordinate.y)
-            Orientation.S -> Coordinate(coordinate.x, coordinate.y -1)
-            Orientation.W -> Coordinate(coordinate.x - 1, coordinate.y)
+            N -> Coordinate(coordinate.x, coordinate.y + 1)
+            E -> Coordinate(coordinate.x + 1, coordinate.y)
+            S -> Coordinate(coordinate.x, coordinate.y -1)
+            W -> Coordinate(coordinate.x - 1, coordinate.y)
         }
         return Position(newCoordinate, orientation)
     }
 
     fun rotateAntiClockwise(): Position {
         val newOrientation = when (orientation) {
-            Orientation.N -> Orientation.W
-            Orientation.E -> Orientation.N
-            Orientation.S -> Orientation.E
-            Orientation.W -> Orientation.S
+            N -> W
+            E -> N
+            S -> E
+            W -> S
         }
         return Position(coordinate, newOrientation)
     }
 
     fun rotateClockwise(): Position {
         val newOrientation = when (orientation) {
-            Orientation.N -> Orientation.E
-            Orientation.E -> Orientation.S
-            Orientation.S -> Orientation.W
-            Orientation.W -> Orientation.N
+            N -> E
+            E -> S
+            S -> W
+            W -> N
         }
         return Position(coordinate, newOrientation)
     }
---
+++ src/test/kotlin/com/tomkp/PositionTest.kt
@@ -0,0 +1,32 @@
+package com.tomkp
+
+import com.tomkp.Orientation.*
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class PositionTest {
+
+    @Test
+    fun forward() {
+        assertEquals(Position(Coordinate(0, 1), N), Position(Coordinate(0, 0), N).forward())
+        assertEquals(Position(Coordinate(1, 0), E), Position(Coordinate(0, 0), E).forward())
+        assertEquals(Position(Coordinate(0, -1), S), Position(Coordinate(0, 0), S).forward())
+        assertEquals(Position(Coordinate(-1, 0), W), Position(Coordinate(0, 0), W).forward())
+    }
+
+    @Test
+    fun rotateAntiClockwise() {
+        assertEquals(Position(Coordinate(0, 0), W), Position(Coordinate(0, 0), N).rotateAntiClockwise())
+        assertEquals(Position(Coordinate(0, 0), N), Position(Coordinate(0, 0), E).rotateAntiClockwise())
+        assertEquals(Position(Coordinate(0, 0), E), Position(Coordinate(0, 0), S).rotateAntiClockwise())
+        assertEquals(Position(Coordinate(0, 0), S), Position(Coordinate(0, 0), W).rotateAntiClockwise())
+    }
+
+    @Test
+    fun rotateClockwise() {
+        assertEquals(Position(Coordinate(0, 0), E), Position(Coordinate(0, 0), N).rotateClockwise())
+        assertEquals(Position(Coordinate(0, 0), S), Position(Coordinate(0, 0), E).rotateClockwise())
+        assertEquals(Position(Coordinate(0, 0), W), Position(Coordinate(0, 0), S).rotateClockwise())
+        assertEquals(Position(Coordinate(0, 0), N), Position(Coordinate(0, 0), W).rotateClockwise())
+    }
+}
