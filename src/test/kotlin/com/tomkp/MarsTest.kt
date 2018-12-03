0d47b0752b86655e8d9e2132259e14d11b8b7b87
tomkp <tom@tomkp.com>
2018-12-03 21:36:21 +0000

    refactor: rename test name, split into 2 tests

--- src/test/kotlin/com/tomkp/MarsTest.kt
+++ src/test/kotlin/com/tomkp/MarsTest.kt
@@ -7,13 +7,17 @@ import org.junit.Test
 class MarsTest {
 
     @Test
-    fun `should return true if a coordinate is on the planet`() {
+    fun `should return true when a coordinate is on the planet`() {
         val mars = Mars(1, 1)
         assertTrue(mars.isOnPlanet(Coordinate(0, 0)))
         assertTrue(mars.isOnPlanet(Coordinate(0, 1)))
         assertTrue(mars.isOnPlanet(Coordinate(1, 0)))
         assertTrue(mars.isOnPlanet(Coordinate(1, 1)))
+    }
 
+    @Test
+    fun `should return false when a coordinate is NOT on the planet`() {
+        val mars = Mars(1, 1)
         assertFalse(mars.isOnPlanet(Coordinate(-1, 0)))
         assertFalse(mars.isOnPlanet(Coordinate(0, -1)))
         assertFalse(mars.isOnPlanet(Coordinate(2, 0)))
@@ -21,7 +25,7 @@ class MarsTest {
     }
 
     @Test
-    fun `should return true if a coordinate has no scent`() {
+    fun `should return true when a coordinate has no scent`() {
         val mars = Mars(1, 1)
         mars.addScent(Coordinate(0, 1))
         assertTrue(mars.hasNoScent(Coordinate(0, 0)))
